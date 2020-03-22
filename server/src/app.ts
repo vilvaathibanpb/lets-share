4aa7cb904f4b38517864000b264fdab723524d1b
Ameer Jhan <ameerjhanprof@gmail.com>
2020-03-22 09:52:24 +0530

    feat: add postgres for db

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,8 @@ import morgan from 'morgan';
 import dotenv from 'dotenv';
 import fs from 'fs';
 import path from 'path';
+import { Sequelize } from 'sequelize';
+import { initUser } from './models/user';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -16,6 +18,34 @@ if (fs.existsSync(envFilePath)) {
 app.use(morgan('short'));
 // json handling middleware
 app.use(express.json());
+
+// initialize the postgress connection
+const sequelize = new Sequelize({
+  database: process.env.DB_NAME,
+  host: `${process.env.DB_HOST}`,
+  username: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  dialect: 'postgres'
+});
+
+// initialize all the models
+initUser(sequelize);
+
+// authenticate and test the connection
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log(
+      `connected to the postgress at ${process.env.DB_HOST} as ${process.env.DB_USER} 💃`
+    );
+  })
+  .catch((err) => {
+    console.log(
+      `😟 unable to connect to postgress at ${process.env.DB_HOST} as ${process.env.DB_USER}: ${err}`
+    );
+  });
+
+// initialize models
 
 if (process.env.NODE_ENV === 'development') {
   console.log('express server is running in devlopment mode 🔨');
---
+++ server/src/models/user.ts
@@ -0,0 +1,41 @@
+import { Model, Sequelize, DataTypes } from 'sequelize';
+
+export class User extends Model {
+  public id!: number;
+  public name!: string;
+  public pincode!: string;
+  public address!: string;
+  public contact!: string;
+}
+
+export function initUser(sequelize: Sequelize) {
+  User.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      pincode: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      address: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      contact: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    },
+    {
+      tableName: 'users',
+      sequelize: sequelize
+    }
+  );
+}
