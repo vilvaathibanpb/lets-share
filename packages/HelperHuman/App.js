7029f20bdd8d8784689293a8c40184966b031a3c
Ameer Jhan <ameerjhanprof@gmail.com>
2020-03-29 15:47:30 +0530

    feat: add babel plugin to read all the translations

--- packages/HelperHuman/App.js
+++ packages/HelperHuman/App.js
@@ -3,14 +3,15 @@ import React, { useState, useEffect } from 'react';
 import { ThemeProvider, theme, colors } from 'react-native-design-system';
 import Router from './src/Router';
 import { IntlProvider } from 'react-intl';
+import { TRANSLATIONS } from './translations';
+
 theme.brandColor.primary = colors.green[700];
 
 export default function App() {
   const [language, setLanguage] = useState("en");
   const [messages, setMessages] = useState({});
-  // useEffect(() => {
-  //   import(`./src/translations/${language}.json`).then((res) => setMessages(res));
-  // }, [language]);
+  
+  console.log(TRANSLATIONS);
 
   return (
     <ThemeProvider value={theme}>
